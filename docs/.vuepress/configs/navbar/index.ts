9d744104befabe527b716fda3052aa6250483a8f
Tyric <2395234635@qq.com>
2021-03-09 17:14:56 +0800

    refactor: navbar

--- docs/.vuepress/configs/navbar/index.ts
+++ docs/.vuepress/configs/navbar/index.ts
@@ -2,16 +2,26 @@ import type { NavbarConfig } from '@vuepress/theme-default';
 
 export const navbar: NavbarConfig = [
   {
-    text: 'ECMAScript',
-    link: '/ecmascript/data-structures.md'
+    text: '编程语言',
+    children: [
+      {
+        text: 'ECMAScript',
+        link: '/ecmascript/data-structures.md'
+      },
+      {
+        text: 'TypeScript',
+        link: '/typescript/type&interface.md'
+      },
+    ]
   },
   {
-    text: 'WebApi',
-    link: '/webapi/clipboard.md'
-  },
-  {
-    text: 'TypeScript',
-    link: '/typescript/type&interface.md'
+    text: '前端',
+    children: [
+      {
+        text: 'WepApi',
+        link: '/webapi/clipboard.md'
+      }
+    ]
   },
   {
     text: 'github',
